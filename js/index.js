f083358fbbf3d84637438cc66a871f53575b9b81
rohitmusti <rohit.musti.rm@gmail.com>
2018-01-11 15:21:54 -0500

    Re-arranged card+

--- js/index.js
+++ js/index.js
@@ -4,17 +4,18 @@ function returnData(data){
   for (i = 0; i < data.length; i++) {
     ret += `<div id='grabPhrase'>` + data[i].grab_phrase + `</div>`
     ret += `<div id='job'>` + data[i].job + `</div>`
+    ret += `<div id='degrees'>` + data[i].degree + `</div>`
     ret += `<ul id='bullets'>`
 
     for (j = 0; j < data[i].descriptions.length; j++) {
       ret += `<li id='description'>` + data[i].descriptions[j] + `</li>`
     }
     ret += `</ul>`
-    ret += `<div id='degrees'>` + data[i].degree + `</div>`
+    ret += `<br />`
+
   }
 
   ret += `</div>`
-
   return ret
   // return "michael is here"
 }
