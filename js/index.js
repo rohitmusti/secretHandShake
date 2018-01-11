bc95df47fc8e589ceb4c88f3a4542f0563c6076f
rohitmusti <rohit.musti.rm@gmail.com>
2018-01-11 15:15:40 -0500

    Finally got it to render the JSON data to the page correctly

--- js/index.js
+++ js/index.js
@@ -1,14 +1,31 @@
 function returnData(data){
-  string ret = "<div>"
+  var ret = `<div id='card'>`
+
   for (i = 0; i < data.length; i++) {
-    ret += "<div id="grabPhrase">" + data[i].grabPhrase + "</div>"
-    ret += "<div id="job">" + data[i].job + "</div>"
-    ret += "<div id="description">" + data[i].description + "</div>"
-    ret += "<div id="degrees">" + data[i].grabPhrase + "</div>"
+    ret += `<div id='grabPhrase'>` + data[i].grab_phrase + `</div>`
+    ret += `<div id='job'>` + data[i].job + `</div>`
+    ret += `<ul id='bullets'>`
 
-
+    for (j = 0; j < data[i].descriptions.length; j++) {
+      ret += `<li id='description'>` + data[i].descriptions[j] + `</li>`
+    }
+    ret += `</ul>`
+    ret += `<div id='degrees'>` + data[i].degree + `</div>`
   }
-  ret += "</div>"
+
+  ret += `</div>`
 
   return ret
+  // return "michael is here"
+}
+
+const TEMP = document.getElementById("TESTER")
+
+window.onload = () => {
+
+  retData = returnData(jobs)
+  // retData = "michael was here"
+
+  TEMP.innerHTML = retData
+
 }
